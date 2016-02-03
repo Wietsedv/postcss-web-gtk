d1680130638e447fce175ffca76b029080482a2c
Wietse <wietsedvries@gmail.com>
2016-02-03 18:13:13 +0100

    Add a color variable replacer test

--- test.js
+++ test.js
@@ -1,20 +1,20 @@
 import postcss from 'postcss';
 import test    from 'ava';
 
-import plugin from './';
+import webGtk from './';
 
-function run(t, input, output, opts = { }) {
-    return postcss([ plugin(opts) ]).process(input)
-        .then( result => {
+function run(t, input, output, opts = {}) {
+    return postcss([webGtk(opts)]).process(input)
+        .then(result => {
             t.same(result.css, output);
             t.same(result.warnings().length, 0);
         });
 }
 
-/* Write tests here
-
-test('does something', t => {
-    return run(t, 'a{ }', 'a{ }', { });
+test('Replace color variables with values', t => {
+    var input = 'div{color:@text_color;background-color: @background_color;}' +
+        '@define-color text_color #fff;' +
+        '@define-color background_color rgba(0, 0,0,0.5);';
+    var output = 'div{color:#fff;background-color: rgba(0, 0,0,0.5);}';
+    return run(t, input, output, {});
 });
-
-*/
